a934edff5cce920b52302b720147fa8c121ab3f1
Dios Vo <51939323+diosvo@users.noreply.github.com>
2020-12-03 18:59:34 +0700

    update styles

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { NumberOnly } from '../directives/number-only.directive';
+import { CharacterOnly } from '../directives/character-only.directive';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent, NumberOnly, CharacterOnly],
   imports: [
     CommonModule,
     AuthRoutingModule,
@@ -21,7 +28,13 @@ import { RegisterComponent } from './register/register.component';
 
     MatCardModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatIconModule
   ]
 })
 export class AuthModule { }
