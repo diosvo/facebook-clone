69d3f4671675297d8439e83e8f0d95c724e097e0
Dios Vo <51939323+diosvo@users.noreply.github.com>
2020-12-29 15:59:12 +0700

    Update UI post

--- src/app/pages/home/home-body/main-body/forum/forum.component.ts
+++ src/app/pages/home/home-body/main-body/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forum',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum.component.scss']
 })
 export class ForumComponent implements OnInit {
+  postForm: FormGroup
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.postForm = this._fb.group({
+      textPost: ['']
+    })
+
+    this.onSubmit()
+  }
+
+  onSubmit() {
+    console.log(this.textPost.value)
+  }
+
+  get textPost() {
+    return this.postForm.get('textPost')
   }
 
 }
